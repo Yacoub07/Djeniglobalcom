1b7b9e46f0141b4b214b81c1a90d88dd599a0cee
Yacoub07 <123514441+Yacoub07@users.noreply.github.com>
2024-09-09 12:28:18 +0000

    Add files via upload

---
+++ Djeniglobalcommerce/src/main/java/com/Djeniglobalcloud/Djeniglobalcommerce/DjeniglobalcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.Djeniglobalcloud.Djeniglobalcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjeniglobalcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjeniglobalcommerceApplication.class, args);
+	}
+
+}
---
+++ Djeniglobalcommerce/src/test/java/com/Djeniglobalcloud/Djeniglobalcommerce/DjeniglobalcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Djeniglobalcloud.Djeniglobalcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjeniglobalcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
